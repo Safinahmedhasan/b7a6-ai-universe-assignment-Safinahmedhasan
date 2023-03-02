dd2d43f784ebc70b8f1c9107db4f68443e8cb4d2
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-03-03 01:56:05 +0600

    js-main-section-add

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const loadData = () => {
+    const URL = ('https://openapi.programming-hero.com/api/ai/tools')
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => showCountris(data.data.tools.slice(0, 6)))
+    
+}
+
+const showCountris = aiUniverse =>{
+    const aiUniverseContainer = document.getElementById('dataContainer')
+    // aiUniverse = aiUniverse.slice(0,6);
+    aiUniverse.forEach(universe => {
+        const universeDiv = document.createElement('div')
+        universeDiv.classList.add('col');
+        universeDiv.innerHTML = `
+        <div class="card h-100">
+                <img src="${universe.image}" class="card-img-top p-3 rounded" alt="...">
+            <div class="card-body p-3">
+                <h5 class="card-title">${universe.name}</h5>
+                <p class="card-text">${universe.description}</p>
+            </div>
+            <div class="card-footer p-3">
+                <h5 class="card-title">${universe.name}</h5>
+                 <small class="text-muted"><i class="fa-regular fa-calendar-days"></i> ${universe.published_in}</small>
+
+            </div>
+      </div>
+        
+        `;
+
+        aiUniverseContainer.appendChild(universeDiv);
+    })
+}
+
+
+
+
+const toggleSpinner = isLoading => {
+    const loadSection = document.getElementById('spinner')
+    if(isLoading){
+        loaderSection.classList.remove('d.none ')
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+loadData(); 
